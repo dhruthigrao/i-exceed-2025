11330bc79bfadcab0cbcd4d109f58919370a60fb
dhruthigrao <115713701+dhruthigrao@users.noreply.github.com>
2025-08-07 10:07:20 +0530

    Add files via upload

---
+++ day2/demoinher.java
@@ -0,0 +1,39 @@
+package day2;
+
+class sample
+{
+    String name,dept;
+    int age;
+
+    sample(String name,String dept,int age)
+    {
+        this.name=name;
+        this.dept=dept;
+        this.age=age;
+
+    }
+
+    public void displayStudent()
+    {
+        System.out.println("name="+name);
+        System.out.println("age="+age);
+        System.out.println("Dept="+dept);
+    }
+}
+
+class teacher extends sample
+{
+    teacher(String name,String dept,int age)
+    {
+        super(name, dept, age);
+        System.out.println("Name of the teacher"+name);
+    }
+}
+public class demoinher {
+    public static void main(String[] args) {
+        teacher("Ram","CS",45);
+    }
+
+
+    
+}
