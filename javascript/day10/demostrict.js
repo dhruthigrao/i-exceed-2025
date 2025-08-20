bd6d2e0d082b2947464bfb61649bc8c92c4e3e64
dhruthigrao <115713701+dhruthigrao@users.noreply.github.com>
2025-08-20 17:20:59 +0530

    Add files via upload

---
+++ javascript/day10/demostrict.js
@@ -0,0 +1,7 @@
+'use strict';
+function addition(x,x)
+{
+    return x+x;
+}
+ 
+console.log(addition(10,20));
---
+++ javascript/day10/demovalue.js
@@ -0,0 +1,12 @@
+var isAlive=false;
+ 
+console.log(typeof(isAlive));
+ 
+if(isAlive)
+{
+    console.log("is Alive");
+}
+else{
+    console.log("gone");
+}
+ 
