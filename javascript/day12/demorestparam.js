d7dcd30675221af15211d3ed43ceb7028661b3b1
dhruthigrao <115713701+dhruthigrao@users.noreply.github.com>
2025-08-20 18:00:20 +0530

    Add files via upload

---
+++ javascript/day12/demorestparam.js
@@ -0,0 +1,14 @@
+function displayData(name,age,city,...args)
+{
+ console.log(name);
+ console.log(age);
+ console.log(city);
+ 
+ console.log(arguments);
+ console.count(displayData);
+ console.log(args.sort());
+ 
+}
+displayData("sathish",43,"salem","Tamilnadu","India","Asia");
+ 
+ 
