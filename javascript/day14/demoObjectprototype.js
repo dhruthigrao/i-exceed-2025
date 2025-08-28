3f40873c0583876751ecdbf125721162be217db6
dhruthigrao <115713701+dhruthigrao@users.noreply.github.com>
2025-08-28 10:00:42 +0530

    Add files via upload

---
+++ javascript/day14/demoObjectprototype.js
@@ -0,0 +1,49 @@
+const bankaccount={
+    balance:1000,
+    deposit:function(amt)
+            {
+                this.balance+=amt;
+            },
+    withdraw:function(amt)
+    {
+        this.balance-=amt;
+    },
+    getBalance:function(amt)
+    {
+        return this.balance;
+    }
+ 
+}
+ 
+let savingaccount=Object.create(bankaccount);
+savingaccount.accno="sb001";
+savingaccount.accholder="Rakesh";
+savingaccount.bank="hdfc";
+savingaccount.branch="bangalore BTM";
+ 
+let currentaccount=Object.create(bankaccount);
+currentaccount.accno="cb001";
+currentaccount.accholder="Rakesh";
+currentaccount.bank="hdfc";
+currentaccount.branch="bangalore BTM";
+ 
+console.log(savingaccount.accholder)
+savingaccount.deposit(120000);
+savingaccount.withdraw(21000);
+console.log(savingaccount.getBalance());
+
+
+let businessbankaccount=Object.create(bankaccount,{accno:{value:"bba01",configurable:true,writable:true,enumerable:true},holder:{value:"dolly",writable:false,configurable:true,enumerable:true},balance:{value:1,writable:true}});
+businessbankaccount.holder="abcd";
+console.log(businessbankaccount.holder);
+console.log(businessbankaccount.balance);
+businessbankaccount.deposit(200000);
+console.log(businessbankaccount.balance);
+businessbankaccount.withdraw(50001);
+console.log(businessbankaccount.balance);
+
+
+for (key in businessbankaccount)
+{
+    console.log(key+":"+businessbankaccount[key])
+}
